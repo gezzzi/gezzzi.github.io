412277f8ea3dff8cd5b851f7b7ad2e4573ab7651
gezzzi <146487115+gezzzi@users.noreply.github.com>
2025-03-12 19:46:19 +0900

    GitHub Pages用の静的エクスポート設定を更新

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   basePath: "",
   images: { unoptimized: true },
   // GitHub Pagesではサブパスがリポジトリ名になるため、本番環境では設定が必要
-  assetPrefix: process.env.NODE_ENV === "production" ? "/gezzzi-github-io" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/io" : "",
 };
 
 module.exports = nextConfig;
